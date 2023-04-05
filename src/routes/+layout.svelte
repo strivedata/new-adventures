eebf3615417adccb2b232d8d4fd2425cd3d3b5b7
Adam Kassama <adam@strivestudio.de>
2023-04-05 20:14:35 +0200

    make it prettier

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,62 @@
 	import './styles.css';
 </script>
 
+<!-- ========== HEADER ========== -->
+<header
+	class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b text-sm py-2.5 sm:py-4 dark:bg-slate-900 dark:border-gray-700">
+	<nav
+		class="max-w-7xl flex basis-full items-center w-full mx-auto px-4 sm:px-6 lg:px-8"
+		aria-label="Global">
+		<div class="mr-5 md:mr-8">
+			<img
+				src="https://uploads-ssl.webflow.com/63e5ff4d535a98822fc01b69/642b4942d4d87f5a2688df45_ibot.png"
+				alt="Logo"
+				width="80px" />
+		</div>
+
+		<div
+			class="w-full flex items-center justify-end ml-auto sm:justify-between sm:gap-x-3 sm:order-3">
+			<div class="sm:hidden">
+				<button
+					type="button"
+					class="inline-flex flex-shrink-0 justify-center items-center gap-2 h-[2.375rem] w-[2.375rem] rounded-full font-medium bg-white text-gray-700 align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition-all text-xs dark:bg-gray-800 dark:hover:bg-slate-800 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-700 dark:focus:ring-offset-gray-800">
+					<svg
+						class="w-3.5 h-3.5"
+						xmlns="http://www.w3.org/2000/svg"
+						width="16"
+						height="16"
+						fill="currentColor"
+						viewBox="0 0 16 16">
+						<path
+							d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+					</svg>
+				</button>
+			</div>
+
+			<div class="hidden sm:block">
+				<label for="icon" class="sr-only" />
+			</div>
+
+			<div class="flex flex-row items-center justify-end gap-2">
+				<div
+					class="hs-dropdown relative inline-flex"
+					data-hs-dropdown-placement="bottom-right">
+					<button
+						id="hs-dropdown-with-header"
+						type="button"
+						class="hs-dropdown-toggle inline-flex flex-shrink-0 justify-center items-center gap-2 h-[2.375rem] w-[2.375rem] rounded-full font-medium bg-white text-gray-700 align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition-all text-xs dark:bg-gray-800 dark:hover:bg-slate-800 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-700 dark:focus:ring-offset-gray-800">
+						<img
+							class="inline-block h-[2.375rem] w-[2.375rem] rounded-full ring-2 ring-white dark:ring-gray-800"
+							src="https://uploads-ssl.webflow.com/63e5ff4d535a98822fc01b69/642b4a1ca2fa0304137b879e_ai3.png"
+							alt="Avatar" />
+					</button>
+				</div>
+			</div>
+		</div>
+	</nav>
+</header>
+<!-- ========== END HEADER ========== -->
+
 <div class="app">
 	<main>
 		<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,78 +3,14 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<!-- ========== HEADER ========== -->
-<header
-	class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b text-sm py-2.5 sm:py-4 dark:bg-slate-900 dark:border-gray-700"
->
-	<nav
-		class="max-w-7xl flex basis-full items-center w-full mx-auto px-4 sm:px-6 lg:px-8"
-		aria-label="Global"
-	>
-		<div class="mr-5 md:mr-8">
-			<img
-				src="https://uploads-ssl.webflow.com/63e5ff4d535a98822fc01b69/642b4942d4d87f5a2688df45_ibot.png"
-				alt="Logo"
-				width="80px"
-			/>
-		</div>
-
-		<div
-			class="w-full flex items-center justify-end ml-auto sm:justify-between sm:gap-x-3 sm:order-3"
-		>
-			<div class="sm:hidden">
-				<button
-					type="button"
-					class="inline-flex flex-shrink-0 justify-center items-center gap-2 h-[2.375rem] w-[2.375rem] rounded-full font-medium bg-white text-gray-700 align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition-all text-xs dark:bg-gray-800 dark:hover:bg-slate-800 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-700 dark:focus:ring-offset-gray-800"
-				>
-					<svg
-						class="w-3.5 h-3.5"
-						xmlns="http://www.w3.org/2000/svg"
-						width="16"
-						height="16"
-						fill="currentColor"
-						viewBox="0 0 16 16"
-					>
-						<path
-							d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
-						/>
-					</svg>
-				</button>
-			</div>
-
-			<div class="hidden sm:block">
-				<label for="icon" class="sr-only" />
-			</div>
-
-			<div class="flex flex-row items-center justify-end gap-2">
-				<div class="hs-dropdown relative inline-flex" data-hs-dropdown-placement="bottom-right">
-					<button
-						id="hs-dropdown-with-header"
-						type="button"
-						class="hs-dropdown-toggle inline-flex flex-shrink-0 justify-center items-center gap-2 h-[2.375rem] w-[2.375rem] rounded-full font-medium bg-white text-gray-700 align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition-all text-xs dark:bg-gray-800 dark:hover:bg-slate-800 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-700 dark:focus:ring-offset-gray-800"
-					>
-						<img
-							class="inline-block h-[2.375rem] w-[2.375rem] rounded-full ring-2 ring-white dark:ring-gray-800"
-							src="https://uploads-ssl.webflow.com/63e5ff4d535a98822fc01b69/642b4a1ca2fa0304137b879e_ai3.png"
-							alt="Avatar"
-						/>
-					</button>
-				</div>
-			</div>
-		</div>
-	</nav>
-</header>
-<!-- ========== END HEADER ========== -->
-
 <div>
 	<!-- Background -->
 	<div class="relative overflow-hidden bg-orange-700 pb-16">
 		<div
 			aria-hidden="true"
-			class="inset-y-0 absolute inset-x-0 left-1/2 w-full -translate-x-1/2 transform overflow-hidden lg:inset-y-0"
-		>
+			class="inset-y-0 absolute inset-x-0 left-1/2 w-full -translate-x-1/2 transform overflow-hidden lg:inset-y-0">
 			<div class="absolute inset-0 flex">
-				<div class="h-full w-full bg-gradient-to-r from-orange-100 to-orange-300" />
+				<div class="h-full w-full bg-gradient-to-r from-cyan-500 to-cyan-800" />
 			</div>
 		</div>
 		<header class="relative py-10">
@@ -82,11 +18,12 @@
 		</header>
 	</div>
 
-	<main class="relative -mt-12">
+	<div class="relative -mt-12">
 		<div class="mx-auto max-w-6xl px-4 pb-6 sm:px-6 lg:px-8 lg:pb-16">
 			<!-- Question Input -->
 			<div class="overflow-hidden rounded-lg bg-white shadow">
-				<div class="divide-y divide-gray-200 lg:grid lg:grid-cols-12 lg:divide-x lg:divide-y-0">
+				<div
+					class="divide-y divide-gray-200 lg:grid lg:grid-cols-12 lg:divide-x lg:divide-y-0">
 					<form class="divide-y divide-gray-200 lg:col-span-12" action="#" method="POST">
 						<div class="px-4 py-4">
 							<!-- Question Input -->
@@ -96,17 +33,14 @@
 										type="text"
 										name="hs-search-box-with-loading-1"
 										class="py-3 lg:px-4 block w-full border-none focus:outline-none font-light rounded-md text-base lg:text-2xl"
-										placeholder="What is your question?"
-									/>
+										placeholder="What is your question?" />
 									<!-- Loading state TODO:Remove hidden class-->
 									<div
-										class="absolute inset-y-0 left-0 flex items-center pointer-events-none pl-4 hidden"
-									>
+										class="absolute inset-y-0 left-0 flex items-center pointer-events-none pl-4 hidden">
 										<div
 											class="animate-spin inline-block w-4 h-4 border-[3px] border-current border-t-transparent text-blue-600 rounded-full"
 											role="status"
-											aria-label="loading"
-										>
+											aria-label="loading">
 											<span class="sr-only">Loading...</span>
 										</div>
 									</div>
@@ -115,8 +49,7 @@
 								<div class="col-span-2 flex justify-end">
 									<button
 										type="button"
-										class="py-2 lg:py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-orange-600 text-white"
-									>
+										class="py-2 lg:py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-cyan-600 text-white">
 										Ask <span class="hidden sm:block">a question</span>
 									</button>
 								</div>
@@ -128,55 +61,71 @@
 
 			<!-- Answers -->
 			<div class="flex flex-col space-y-4 mt-12">
-
 				<!-- Single Answers Card -->
 				<div class="bg-white px-4 py-5 sm:px-6 rounded-lg shadow max-w-2xl">
 					<article class="group relative flex flex-col items-start px-4 mt-6">
-
 						<!-- Questions -->
 						<blockquote class="relative">
-							<svg class="absolute top-0 left-4 transform -translate-x-6 -translate-y-8 h-16 w-16 text-gray-100 dark:text-gray-700" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-							  <path d="M7.39762 10.3C7.39762 11.0733 7.14888 11.7 6.6514 12.18C6.15392 12.6333 5.52552 12.86 4.76621 12.86C3.84979 12.86 3.09047 12.5533 2.48825 11.94C1.91222 11.3266 1.62421 10.4467 1.62421 9.29999C1.62421 8.07332 1.96459 6.87332 2.64535 5.69999C3.35231 4.49999 4.33418 3.55332 5.59098 2.85999L6.4943 4.25999C5.81354 4.73999 5.26369 5.27332 4.84476 5.85999C4.45201 6.44666 4.19017 7.12666 4.05926 7.89999C4.29491 7.79332 4.56983 7.73999 4.88403 7.73999C5.61716 7.73999 6.21938 7.97999 6.69067 8.45999C7.16197 8.93999 7.39762 9.55333 7.39762 10.3ZM14.6242 10.3C14.6242 11.0733 14.3755 11.7 13.878 12.18C13.3805 12.6333 12.7521 12.86 11.9928 12.86C11.0764 12.86 10.3171 12.5533 9.71484 11.94C9.13881 11.3266 8.85079 10.4467 8.85079 9.29999C8.85079 8.07332 9.19117 6.87332 9.87194 5.69999C10.5789 4.49999 11.5608 3.55332 12.8176 2.85999L13.7209 4.25999C13.0401 4.73999 12.4903 5.27332 12.0713 5.85999C11.6786 6.44666 11.4168 7.12666 11.2858 7.89999C11.5215 7.79332 11.7964 7.73999 12.1106 7.73999C12.8437 7.73999 13.446 7.97999 13.9173 8.45999C14.3886 8.93999 14.6242 9.55333 14.6242 10.3Z" fill="currentColor"/>
+							<svg
+								class="absolute top-0 left-4 transform -translate-x-6 -translate-y-8 h-16 w-16 text-gray-100 dark:text-gray-700"
+								width="16"
+								height="16"
+								viewBox="0 0 16 16"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+								aria-hidden="true">
+								<path
+									d="M7.39762 10.3C7.39762 11.0733 7.14888 11.7 6.6514 12.18C6.15392 12.6333 5.52552 12.86 4.76621 12.86C3.84979 12.86 3.09047 12.5533 2.48825 11.94C1.91222 11.3266 1.62421 10.4467 1.62421 9.29999C1.62421 8.07332 1.96459 6.87332 2.64535 5.69999C3.35231 4.49999 4.33418 3.55332 5.59098 2.85999L6.4943 4.25999C5.81354 4.73999 5.26369 5.27332 4.84476 5.85999C4.45201 6.44666 4.19017 7.12666 4.05926 7.89999C4.29491 7.79332 4.56983 7.73999 4.88403 7.73999C5.61716 7.73999 6.21938 7.97999 6.69067 8.45999C7.16197 8.93999 7.39762 9.55333 7.39762 10.3ZM14.6242 10.3C14.6242 11.0733 14.3755 11.7 13.878 12.18C13.3805 12.6333 12.7521 12.86 11.9928 12.86C11.0764 12.86 10.3171 12.5533 9.71484 11.94C9.13881 11.3266 8.85079 10.4467 8.85079 9.29999C8.85079 8.07332 9.19117 6.87332 9.87194 5.69999C10.5789 4.49999 11.5608 3.55332 12.8176 2.85999L13.7209 4.25999C13.0401 4.73999 12.4903 5.27332 12.0713 5.85999C11.6786 6.44666 11.4168 7.12666 11.2858 7.89999C11.5215 7.79332 11.7964 7.73999 12.1106 7.73999C12.8437 7.73999 13.446 7.97999 13.9173 8.45999C14.3886 8.93999 14.6242 9.55333 14.6242 10.3Z"
+									fill="currentColor" />
 							</svg>
-						  
+
 							<div class="relative z-10">
-							  <p class="text-gray-800 sm:text-xl dark:text-white"><em>
-								Was genau sind Strive Days?
-							  </p>
+								<p class="text-gray-800 sm:text-xl dark:text-white">
+									<em> Was genau sind Strive Days? </em>
+								</p>
 							</div>
-						  </blockquote>
+						</blockquote>
 
 						<!-- Answer -->
 						<div class="">
 							<p class="relative z-10 mt-2 text-base text-zinc-500 font-light">
-								Strive Days sind Meetings die stattfinden bei denen Themen besprochen werden die sich nicht für online Meetings eignen. Bisher in Nürnberg, Würzburg und Berlin. Immer auch mit etwas Spaß dabei.
+								Strive Days sind Meetings die stattfinden bei denen Themen
+								besprochen werden die sich nicht für online Meetings eignen. Bisher
+								in Nürnberg, Würzburg und Berlin. Immer auch mit etwas Spaß dabei.
 							</p>
 						</div>
 
-
 						<div
 							aria-hidden="true"
-							class="relative z-10 mt-4 flex items-center text-sm font-light text-gray-300"
-						>
+							class="relative z-10 mt-4 flex items-center text-sm font-light text-gray-300">
 							Frage vom 12. Januar, 5:35 am
 						</div>
 					</article>
-				</div>				
+				</div>
 			</div>
 
 			<!-- CTA -->
-			<div class="bg-orange-50 rounded-lg mt-24">
-				<div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between lg:px-8">
-					<h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Bereit AI zu wagen?<br>Buche einen Video-Call mit uns.</h2>
-					<div class="mt-10 flex flex-col sm:flex-row items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
-				
-						<a href="https://www.bit.ly/3XBY2b4" target="_blank" class="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm">
+			<div class="bg-cyan-600 rounded-lg mt-24">
+				<div
+					class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between lg:px-8">
+					<h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+						Bereit AI zu wagen?<br />Buche einen Video-Call mit uns.
+					</h2>
+					<div
+						class="mt-10 flex flex-col sm:flex-row items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
+						<a
+							href="https://www.bit.ly/3XBY2b4"
+							target="_blank"
+							class="rounded-md bg-white px-3.5 py-3.5 text-base font-semibold text-gray-800 shadow-sm">
 							Jetzt Call buchen (30min)</a>
-						<a href="https://www.strivestudio.de" target="_blank" class="mt-4 sm:mt-0 text-sm font-semibold leading-6 text-gray-900">Erfahre mehr über Strive <span aria-hidden="true">→</span></a>
+						<a
+							href="https://www.strivestudio.de"
+							target="_blank"
+							class="mt-4 sm:mt-0 text-sm font-semibold leading-6 text-white">
+							Strive Studio <span aria-hidden="true">→</span></a>
 					</div>
 				</div>
 			</div>
 		</div>
-		  
-	</main>
+	</div>
 </div>
